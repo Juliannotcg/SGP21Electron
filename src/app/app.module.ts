15ec053e8aff63d1e30eb8977dbe9fa85507ea08
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-05-05 21:07:05 -0300

    Inicio do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent]
+})
+export class AppModule { }
